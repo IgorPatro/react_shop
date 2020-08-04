d62ae5f3da54de999cfa9ff35dca72e0f0c4f561
Igor Patro <i.patro@wp.pl>
2020-08-04 16:07:45 +0200

    installed styled-components & added some rules in eslint config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import GlobalStyle from 'theme/globalStyle';
 
-const App = () => <h1>Hello world and react shops!</h1>;
+const App = () => (
+  <>
+    <GlobalStyle />
+    <h1>Hello world and react shops!</h1>
+  </>
+);
 
 export default App;
